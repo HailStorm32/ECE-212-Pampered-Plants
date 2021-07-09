dab6d6f8eea9f0a441aa88587f5bc256bb7f54d3
Demetri <demetriv.s.7@gmail.com>
2021-07-08 20:33:54 -0700

    Added file for program functions +++ Added comments to file explaining proper comment blocks

---
+++ MainBoard/functions.h
@@ -0,0 +1,34 @@
+/*
+ * Please use the following comment block above your fuction:
+ */
+
+ /*
+  * Description: Should provide a breif description of
+  *   what the fuction does
+  *  
+  * Arguments:
+  *   (INPUT/OUTPUT) argumentName -- short one line description of argument
+  *   
+  *  Return:
+  *    returnType(void, int, uint8_t...) -- short one line description of what it values it returns
+  */
+
+  //EXAMPLE
+
+  /*
+   * Description: sends a pre packaged message to the given address using the
+   *  address graph (if it has been set up) to send out messages
+   *  
+   * Arguments:
+   *   (INPUT) radioP -- pointer to radio instance
+   *   (INPUT) packedMessage -- prepacked message to send
+   *   (INPUT) receivingAddr -- address to send message to
+   *   
+   * Return:
+   *  true -- was able to send to a hub
+   *  false -- wasnt able to send to any hub
+   */
+
+
+
+   //============================= Functions Begin Below This Line ===================================================
