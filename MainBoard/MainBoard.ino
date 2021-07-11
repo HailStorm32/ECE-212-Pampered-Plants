fe5fb31600bafe2a7c50e95cfb296c3b51a25bc5
SirArchibold <56450716+SirArchibold@users.noreply.github.com>
2021-07-11 15:57:21 -0700

    updated MainBoard with readSoil setup

--- MainBoard/MainBoard.ino
+++ MainBoard/MainBoard.ino
@@ -1,8 +1,16 @@
 #include "functions.h"
 
-void setup() {
-
-
+void setup() 
+{
+  Serial.begin(9600);
+  
+  // setup for readSoil function
+  int moisture = 0; // initialize output variable
+  const int soilPin = A2; // set signal pin to analog (any analog pin works)
+  const int soilPower = 7; // sensor powered through digital pin to reduce corrosion (any digital pin works)
+  pinMode(soilPower, OUTPUT); // set digital pin as output
+  digitalWrite(soilPower, LOW); // set default to no power, reduces corrosion on sensor
+  // end setup for readSoil function
 }
 
 
