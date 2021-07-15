1acad3d28932e8e17822b8a591b18b15cf185e97
SirArchibold <56450716+SirArchibold@users.noreply.github.com>
2021-07-15 13:07:20 -0700

    added global variables
    for use with temperature function

--- MainBoard/MainBoard.ino
+++ MainBoard/MainBoard.ino
@@ -3,10 +3,14 @@ const uint8_t SOIL_PIN = A2; // set signal pin to analog (any analog pin works)
 const uint8_t SOIL_PWR_PIN = 7; // sensor powered through digital pin to reduce corrosion (any digital pin works)
 const uint8_t LDR_1_PIN = A0;
 const uint8_t LDR_2_PIN = A1;
+const uint8_t DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 
 //---------------Begin Includes------------------------------------//
 #include "functions.h"
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
   
 void setup() 
 {
