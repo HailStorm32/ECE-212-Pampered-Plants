144e946b30095cc42bc5db336f6241022646b651
Demetri <demetriv.s.7@gmail.com>
2021-07-16 12:18:28 -0700

    Fixed compile error ===Moved function includes section
    ===NOTE: Optimizations are still needed for the temperature function

--- MainBoard/MainBoard.ino
+++ MainBoard/MainBoard.ino
@@ -1,16 +1,21 @@
+//---------------Begin Includes------------------------------------//
+#include <dht_nonblocking.h>
+
+
 //---------------Begin Global Variables-----------------------------//
 const uint8_t SOIL_PIN = A2; // set signal pin to analog (any analog pin works)
 const uint8_t SOIL_PWR_PIN = 7; // sensor powered through digital pin to reduce corrosion (any digital pin works)
 const uint8_t LDR_1_PIN = A0;
 const uint8_t LDR_2_PIN = A1;
 const uint8_t DHT_SENSOR_PIN = 2;
+
+#define DHT_SENSOR_TYPE DHT_TYPE_11
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 
-//---------------Begin Includes------------------------------------//
-#include "functions.h"
-#include <dht_nonblocking.h>
-#define DHT_SENSOR_TYPE DHT_TYPE_11
+#include "functions.h"//This is included here seperatly so that the gobals defined above can be used in the functions.h
+
+
   
 void setup() 
 {
